1db5db8c3a8c9c73b64f327764ea4a9994be0cb4
yujaemin <woawoa0513@gmail.com>
2022-07-29 15:35:37 +0900

    AOP - TimeTrace

--- src/main/java/hello/hellospring/aop/TimeTraceAop.java
+++ src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -7,10 +7,10 @@ import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 @Aspect
-@Component
+@Component // 컴포넌트 등록 대신, AOP 쓴다는 것을 인지할 수 있도록 스프링 빈(SpringConfig)에 등록하는 방법을 선택해도 된다.
 public class TimeTraceAop {
 
-    @Around("execution(* hello.hellospring..*(..))")
+    @Around("execution(* hello.hellospring..*(..))") // AOP 어디에 적용할 것인지 , hello.hellospring 하위 파일에 모두 적용
     public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
         System.out.println("START : " + joinPoint.toString());
